98f131d41a7b4aba99198b29ad39c6693dd6adfb
vandontduong <vandon@stanford.edu>
2023-11-29 16:21:36 -0800

    initialize miscellaneous folder and summary-analyses.sh

---
+++ miscellaneous/summary-analyses.sh
@@ -0,0 +1,8 @@
+#! /oak/stanford/groups/cmackall/vandon/CARTEx/miscellaneous
+
+echo "copying files over to /CARTEx/miscellaneous..."
+
+echo "GSE125881"
+cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group.jpeg .
+cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group_highlight.jpeg .
+
