2f37253f61afe519e3da0c0be463aed829d9e8af
vandontduong <vandon@stanford.edu>
2023-12-01 21:37:26 -0800

    update

--- cartex-utilities.R
+++ cartex-utilities.R
@@ -90,7 +90,7 @@ DimPlotHighlightIdents <- function(atlas, identity, reduction_map, highlight_col
 #####
 # Bar plot stacked and split according to two different identity groups
 
-BarPlotSplit <- function(atlas, x_identity, y_identity){
+BarPlotStackSplit <- function(atlas, x_identity, y_identity){
   md <- as.data.table(atlas@meta.data)[, .N, by = c(x_identity, y_identity)]
   md$percent <- round(100*md$N / sum(md$N), digits = 1)
   barplot <- ggplot(md, aes_string(x = y_identity, y = 'N', fill = x_identity)) + geom_col(position = "fill")
