6246450f1615ada64af179c23a1894faeed6c5d2
vandontduong <vandon@stanford.edu>
2023-12-09 13:03:48 -0800

    add plots for GSE136874

--- miscellaneous/summarized-analyses.sh
+++ miscellaneous/summarized-analyses.sh
@@ -8,5 +8,10 @@ cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group_highlight.jpeg ./
 cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_phase.jpeg ./plots
 cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_phase_highlight.jpeg ./plots
 
+echo "GSE136874"
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CAR.jpeg ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CAR_highlight.jpeg ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase.jpeg ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase_highlight.jpeg ./plots
 
 
