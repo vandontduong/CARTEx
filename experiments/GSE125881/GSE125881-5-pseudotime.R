fb51925ad1c6c2075732040bc85cd80a5d4db644
vandontduong <vandon@stanford.edu>
2024-01-25 19:24:02 -0800

    initialize diffusion

---
+++ experiments/GSE125881/GSE125881-5-pseudotime.R
@@ -0,0 +1,13 @@
+
+
+# https://github.com/kstreet13/slingshot/issues/43
+# http://barcwiki.wi.mit.edu/wiki/SOP/scRNA-seq/Slingshot
+# https://danhdtruong.com/Diffusion-Pseudotime/
+# https://bioconductor.org/packages/release/bioc/vignettes/destiny/inst/doc/DPT.html
+# https://rnabioco.github.io/cellar/previous/2019/docs/5_trajectories.html
+# http://barcwiki.wi.mit.edu/wiki/SOP/scRNA-seq/diffusionMaps
+# https://bioconductor.org/packages/release/bioc/vignettes/destiny/inst/doc/DPT.html
+# https://bioconductor.org/packages/release/bioc/vignettes/destiny/inst/doc/Diffusion-Maps.html
+# https://rnabioco.github.io/cellar/previous/2019/docs/3_norm_viz_clustering.html
+
+
