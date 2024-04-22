971a72b44a2e05032dbf53c789deb160bfb4c01b
vandontduong <vandon@stanford.edu>
2024-04-21 17:15:50 -0700

    re-initialize prepare script

---
+++ experiments/GSE235676/GSE235676-1-prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=GSE235676-prepare
+#SBATCH --output=GSE235676-1-prepare.out
+#SBATCH --error=GSE235676-1-prepare.err
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=500G
+#SBATCH --partition=owners
+
+ml R/4.2.0
+ml biology physics geos/3.6.2 hdf5/1.12.2
+Rscript GSE235676-1-prepare.R
