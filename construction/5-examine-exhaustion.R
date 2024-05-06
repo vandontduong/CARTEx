e715d90799071b19aff9d7f0859cce27ddb0fa1d
vandontduong <vandon@stanford.edu>
2024-05-06 00:06:40 -0700

    update construction

--- construction/5-examine-exhaustion.R
+++ construction/5-examine-exhaustion.R
@@ -1,3 +1,6 @@
+
+
+
 setwd("/oak/stanford/groups/cmackall/vandon/CARTEx/construction")
 set.seed(123)
 source("/oak/stanford/groups/cmackall/vandon/CARTEx/cartex-utilities.R")
@@ -58,6 +61,11 @@ NK_like <- rownames(read.csv(paste(PATH_SIGNATURES, "NK-like-dysfunction.csv", s
 Wherry_Tex <- rownames(read.csv(paste(PATH_SIGNATURES, "Wherry_2007_Immunity_LCMV_Tex_humanized_version.csv", sep = ''), header = TRUE, row.names = 1))
 BBD_Tex <- rownames(read.csv(paste(PATH_SIGNATURES, "Selli_2023_Blood_TBBDex.csv", sep = ''), header = TRUE, row.names = 1))
 PD1_Tex <- rownames(read.csv(paste(PATH_SIGNATURES, "Cai_2020_Pathology_PD1_Tex.csv", sep = ''), header = TRUE, row.names = 1))
+activation.sig <- rownames(read.csv(paste(PATH_SIGNATURES, "panther-activation.csv", sep = ''), header = TRUE, row.names = 1))
+anergy.sig <- rownames(read.csv(paste(PATH_SIGNATURES, "SAFFORD_T_LYMPHOCYTE_ANERGY.csv", sep = ''), header = TRUE, row.names = 1))
+stemness.sig <- rownames(read.csv(paste(PATH_SIGNATURES, "GSE23321_CD8_STEM_CELL_MEMORY_VS_EFFECTOR_MEMORY_CD8_TCELL_UP.csv", sep = ''), header = TRUE, row.names = 1))
+senescence.sig <- rownames(read.csv(paste(PATH_SIGNATURES, "M9143_FRIDMAN_SENESCENCE_UP.csv", sep = ''), header = TRUE, row.names = 1))
+
 
 rownames(mat)
 index_CARTEx_630 <- rownames(mat) %in% rownames(cartex_630_weights)
@@ -67,6 +75,10 @@ index_NKlike <- rownames(mat) %in% NK_like
 index_Wherry <- rownames(mat) %in% Wherry_Tex
 index_BBD <- rownames(mat) %in% BBD_Tex
 index_PD1 <- rownames(mat) %in% PD1_Tex
+index_activation <- rownames(mat) %in% activation.sig
+index_anergy <- rownames(mat) %in% anergy.sig
+index_stemness <- rownames(mat) %in% stemness.sig
+index_senescence <- rownames(mat) %in% senescence.sig
 
 intersect_CARTEx_630 <- rownames(mat)[index_CARTEx_630]
 intersect_CARTEx_200 <- rownames(mat)[index_CARTEx_200]
@@ -75,6 +87,10 @@ intersect_NKlike <- rownames(mat)[index_NKlike]
 intersect_Wherry <- rownames(mat)[index_Wherry]
 intersect_BBD <- rownames(mat)[index_BBD]
 intersect_PD1 <- rownames(mat)[index_PD1]
+intersect_activation <- rownames(mat)[index_activation]
+intersect_anergy <- rownames(mat)[index_anergy]
+intersect_stemness <- rownames(mat)[index_stemness]
+intersect_senescence <- rownames(mat)[index_senescence]
 
 intersect(intersect_CARTEx_630, intersect_NKlike)
 intersect(intersect_Wherry, intersect_NKlike)
@@ -88,7 +104,10 @@ mat_NKlike <- mat[index_NKlike,]
 mat_Wherry <- mat[intersect_Wherry,]
 mat_BBD <- mat[intersect_BBD,]
 mat_PD1 <- mat[intersect_PD1,]
-
+mat_activation <- mat[intersect_activation,]
+mat_anergy <- mat[intersect_anergy,]
+mat_senescence <- mat[intersect_senescence,]
+mat_stemness <- mat[intersect_stemness,]
 
 # plot
 set.seed(123)
@@ -97,59 +116,118 @@ col_order = c('CD19_Donor76_Day11', 'CD19_Donor86_Day11', 'CD19_Donor90_Day11', 
               'HA_Donor76_Day11', 'HA_Donor86_Day11', 'HA_Donor90_Day11', 'HA_Donor76_Day15', 'HA_Donor86_Day15', 'HA_Donor90_Day15', 'HA_Donor76_Day21', 'HA_Donor86_Day21', 'HA_Donor90_Day21',
               'Control_Donor76_Day0', 'Control_Donor86_Day0', 'Control_Donor90_Day0')
 
+colAnn <- HeatmapAnnotation(
+  df = ann,
+  annotation_height = 0.6,
+  annotation_width = unit(1, 'cm'),
+  gap = unit(1, 'mm'), 
+  annotation_legend_param = list(
+    CAR = list(nrow = 2, title = 'CAR',title_position = 'topcenter',legend_direction = 'vertical',title_gp = gpar(fontsize = 12, fontface = 'bold'),labels_gp = gpar(fontsize = 12, fontface = 'bold')),
+    Timepoint = list(nrow = 3,title = 'Timepoint',title_position = 'topcenter',legend_direction = 'vertical',title_gp = gpar(fontsize = 12, fontface = 'bold'),labels_gp = gpar(fontsize = 12, fontface = 'bold'))),
+  col = list(CAR = c("CD19" = "dodgerblue", "HA" = "indianred", "Control" = "darkgoldenrod"),
+             Timepoint = setNames(colorRampPalette(c("lightgrey","lightblue","mediumblue"))(4), c(0,11,15,21)))
+  )
+
+hmap <- Heatmap(mat, 
+                name = 'Zscore',
+                column_split = split, column_order = col_order,
+                row_gap = unit(1.5, "mm"),
+                row_split = pamClusters$clustering, 
+                cluster_row_slices = FALSE,
+                row_dend_reorder = TRUE, 
+                cluster_rows = TRUE, 
+                show_row_dend = TRUE,
+                show_row_names = FALSE,
+                row_names_gp = gpar(fontsize = 10, fontface = 'bold'),
+                row_names_side = 'right',
+                row_dend_width = unit(5,'mm'),
+                column_dend_reorder = FALSE,
+                column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
+                top_annotation = colAnn, 
+                use_raster=FALSE)
+
+# draw(hmap,  annotation_legend_side = 'left', heatmap_legend_side = 'left')
+plot_heatmap_all_clusters <- as.grob(hmap)
+generate_figs(plot_heatmap_all_clusters, './plots/plot_heatmap_all_clusters', c(6,6))
+
+# https://jokergoo.github.io/ComplexHeatmap-reference/book/heatmap-annotations.html
+
 hmap_CARTEx_630 <- Heatmap(mat_CARTEx_630, name = 'Zscore', column_split = split, column_order = col_order,
-                       row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE,
+                       row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_CARTEx_630], cluster_row_slices = FALSE, row_dend_reorder = TRUE,
                        cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                        row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                        top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_CARTEx_630), './plots/plot_heatmap_CARTEx_630', c(10,6))
+generate_figs(as.grob(hmap_CARTEx_630), './plots/plot_heatmap_CARTEx_630', c(6,6))
 
 hmap_CARTEx_200 <- Heatmap(mat_CARTEx_200, name = 'Zscore', column_split = split, column_order = col_order,
-                           row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                           row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_CARTEx_200], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
                            cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                            row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                            top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_CARTEx_200), './plots/plot_heatmap_CARTEx_200', c(10,6))
+generate_figs(as.grob(hmap_CARTEx_200), './plots/plot_heatmap_CARTEx_200', c(6,6))
 
 hmap_CARTEx_84 <- Heatmap(mat_CARTEx_84, name = 'Zscore', column_split = split, column_order = col_order,
-                           row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                           row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_CARTEx_84], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
                            cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                            row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                            top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_CARTEx_84), './plots/plot_heatmap_CARTEx_84', c(10,6))
+generate_figs(as.grob(hmap_CARTEx_84), './plots/plot_heatmap_CARTEx_84', c(6,6))
 
 hmap_NKlike <- Heatmap(mat_NKlike, name = 'Zscore', column_split = split, column_order = col_order,
-                row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_NKlike], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
                 cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                 row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                 top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_NKlike), './plots/plot_heatmap_NKlike', c(10,6))
+generate_figs(as.grob(hmap_NKlike), './plots/plot_heatmap_NKlike', c(6,6))
 
 hmap_Wherry <- Heatmap(mat_Wherry, name = 'Zscore', column_split = split, column_order = col_order,
-                       row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                       row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_Wherry], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
                        cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                        row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                        top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_Wherry), './plots/plot_heatmap_Wherry', c(10,6))
+generate_figs(as.grob(hmap_Wherry), './plots/plot_heatmap_Wherry', c(6,6))
 
 hmap_BBD <- Heatmap(mat_BBD, name = 'Zscore', column_split = split, column_order = col_order,
-                       row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                       row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_BBD], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
                        cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                        row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                        top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_BBD), './plots/plot_heatmap_BBD', c(10,6))
+generate_figs(as.grob(hmap_BBD), './plots/plot_heatmap_BBD', c(6,6))
 
 hmap_PD1 <- Heatmap(mat_PD1, name = 'Zscore', column_split = split, column_order = col_order,
-                    row_gap = unit(1.5, "mm"), cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                    row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_PD1], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
                     cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
                     row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
                     top_annotation = colAnn, use_raster=FALSE)
-generate_figs(as.grob(hmap_PD1), './plots/plot_heatmap_PD1', c(10,6))
+generate_figs(as.grob(hmap_PD1), './plots/plot_heatmap_PD1', c(6,6))
 
+hmap_activation <- Heatmap(mat_activation, name = 'Zscore', column_split = split, column_order = col_order,
+                    row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_activation], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                    cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
+                    row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
+                    top_annotation = colAnn, use_raster=FALSE)
+generate_figs(as.grob(hmap_activation), './plots/plot_heatmap_activation', c(6,6))
 
+hmap_anergy <- Heatmap(mat_anergy, name = 'Zscore', column_split = split, column_order = col_order,
+                           row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_anergy], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                           cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
+                           row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
+                           top_annotation = colAnn, use_raster=FALSE)
+generate_figs(as.grob(hmap_anergy), './plots/plot_heatmap_anergy', c(6,6))
 
+hmap_senescence <- Heatmap(mat_senescence, name = 'Zscore', column_split = split, column_order = col_order,
+                       row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_senescence], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                       cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
+                       row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
+                       top_annotation = colAnn, use_raster=FALSE)
+generate_figs(as.grob(hmap_senescence), './plots/plot_heatmap_senescence', c(6,6))
 
-
+hmap_stemness <- Heatmap(mat_stemness, name = 'Zscore', column_split = split, column_order = col_order,
+                           row_gap = unit(1.5, "mm"), row_split = pamClusters$clustering[index_stemness], cluster_row_slices = FALSE, row_dend_reorder = TRUE, 
+                           cluster_rows = TRUE, show_row_dend = TRUE,show_row_names = FALSE, row_names_gp = gpar(fontsize = 10, fontface = 'bold'), row_names_side = 'right',
+                           row_dend_width = unit(5,'mm'), column_dend_reorder = FALSE, column_names_gp = gpar(fontsize = 8, fontface = 'bold'),
+                           top_annotation = colAnn, use_raster=FALSE)
+generate_figs(as.grob(hmap_stemness), './plots/plot_heatmap_stemness', c(6,6))
 
 
 
---
+++ construction/construction-results.py
@@ -0,0 +1,77 @@
+from pptx import Presentation
+from pptx.util import Inches
+
+experiment = "construction"
+description = "Construction analysis"
+
+prs = Presentation()
+# set width and height to 16 and 9 inches.
+prs.slide_width = Inches(16)
+prs.slide_height = Inches(9)
+
+blank_slide_layout = prs.slide_layouts[6]
+
+def add_slide(prs, layout, subtitle):
+	slide = prs.slides.add_slide(layout)
+	txBox = slide.shapes.add_textbox(left = Inches(0), top = Inches(0), width = Inches(1), height = Inches(1))
+	tf = txBox.text_frame
+	tf.text = experiment + ": " + description
+	p = tf.add_paragraph()
+	p.text = subtitle
+	return slide
+
+def add_text(x, y, statement):
+	txBox = slide.shapes.add_textbox(left = x, top = y, width = Inches(1), height = Inches(0.5))
+	tf = txBox.text_frame
+	tf.text = statement
+
+
+# new slide
+slide = add_slide(prs, blank_slide_layout, "Heatmaps of all genes in experiment")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_all_clusters.png", left = Inches(0.5), top = Inches(1), height = Inches(7))
+
+# new slide
+slide = add_slide(prs, blank_slide_layout, "Heatmaps of exhaustion")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_CARTEx_630.png", left = Inches(0.5), top = Inches(1), height = Inches(2.5))
+add_text(Inches(0.5), Inches(0.6), "CARTEx 630")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_CARTEx_200.png", left = Inches(4.5), top = Inches(1), height = Inches(2.5))
+add_text(Inches(4.5), Inches(0.6), "CARTEx 200")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_CARTEx_84.png", left = Inches(8.5), top = Inches(1), height = Inches(2.5))
+add_text(Inches(8.5), Inches(0.6), "CARTEx 84")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_Wherry.png", left = Inches(0.5), top = Inches(4), height = Inches(2.5))
+add_text(Inches(0.5), Inches(3.6), "LCMV / Wherry")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_NKlike.png", left = Inches(4.5), top = Inches(4), height = Inches(2.5))
+add_text(Inches(4.5), Inches(3.6), "NK-like")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_BBD.png", left = Inches(8.5), top = Inches(4), height = Inches(2.5))
+add_text(Inches(8.5), Inches(3.6), "BBD")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_PD1.png", left = Inches(12.5), top = Inches(4), height = Inches(2.5))
+add_text(Inches(12.5), Inches(3.6), "PD1")
+
+# new slide
+slide = add_slide(prs, blank_slide_layout, "Heatmaps of cell states")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_activation.png", left = Inches(0.5), top = Inches(1), height = Inches(2.5))
+add_text(Inches(0.5), Inches(0.6), "Activation")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_anergy.png", left = Inches(5), top = Inches(1), height = Inches(2.5))
+add_text(Inches(5), Inches(0.6), "Anergy")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_senescence.png", left = Inches(0.5), top = Inches(4), height = Inches(2.5))
+add_text(Inches(0.5), Inches(3.6), "Senescence")
+
+pic = slide.shapes.add_picture("./plots/" + "plot_heatmap_stemness.png", left = Inches(5), top = Inches(4), height = Inches(2.5))
+add_text(Inches(5), Inches(3.6), "Stemness")
+
+prs.save(experiment + '_results.pptx')
+
+
+
+
