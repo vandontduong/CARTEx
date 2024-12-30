cf233deb84b34c16df35cdb2441ffa9fa1a147fe
vandontduong <vandon@stanford.edu>
2024-12-29 20:03:23 -0800

    update misc folder

--- miscellaneous/summarized-analyses.sh
+++ miscellaneous/summarized-analyses.sh
@@ -13,6 +13,7 @@ cp ../construction/plots/bespoke_plot_all_clusters.png ./plots
 cp ../construction/plots/plot_initial_pca.png ./plots
 cp ../construction/plots/plot_canonical_exhaustion_markers.png ./plots
 cp ../construction/plots/plt_CARTEx_200.png ./plots
+cp ../construction/plots/plt_CARTEx_84.png ./plots
 cp ../construction/plots/CARTEx_reactome_coverage.png ./plots
 cp ../construction/plots/CARTEx_reactome_pval.png ./plots
 
@@ -25,7 +26,13 @@ cp ../construction/plots/barplot_significant_pathways.png ./plots
 
 cp ../construction/plots/bespoke_plot_heatmap_all_sigs_hires.png ./plots
 cp ../construction/plots/bespoke_plot_heatmap_legend_alt.png ./plots
+cp ../construction/plots/bespoke_plot_heatmap_all_clusters.png ./plots
 cp ../construction/plots/plot_signature_cluster_representation.png ./plots
+cp ../construction/plots/plot_mean_zscore_by_signature.png ./plots
+cp ../construction/plots/plot_pathways_analysis.png ./plots
+cp ../construction/plots/upset_exhaustion_state_sigs.png ./plots
+cp ../construction/plots/barplot_significant_pathways.png ./plots
+
 
 
 cp ../construction/plots/glmnet_equation.png ./plots
@@ -38,40 +45,128 @@ cp ../construction/plots/bespoke_glmnet_alphasize.png ./plots
 cp ../construction/plots/plt_CARTEx_84.png ./plots
 
 
+cp ../construction/plots/HA_model_diagram.png ./plots
+cp ../construction/plots/plot_weights.png ./plots
+cp ../construction/plots/bespoke_glmnet_multiple_regressions.png ./plots
+cp ../construction/plots/bespoke_glmnet_alphasize.png ./plots
+
+cp ../construction/plots/upset_expt_CARTEx84.png ./plots
+cp ../construction/plots/upset_expt_CARTEx200.png ./plots
+cp ../construction/plots/all_genes_repmap.png ./plots
+cp ../construction/plots/fraction_CARTEx_repchart.png ./plots
+cp ../construction/plots/fraction_sig_repchart_CARTEx.png ./plots
+cp ../construction/plots/ggvenn_compare_exhaustion_sigs.png ./plots
+cp ../construction/plots/upset_exhaustion_differentiation.png ./plots
+
+
+
+
+
 
 echo "GSE125881"
-cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group.jpeg ./plots
-cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group_highlight.jpeg ./plots
-cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_phase.jpeg ./plots
-cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_phase_highlight.jpeg ./plots
-cp ../experiments/GSE125881/plots/GSE125881_prepare_barplot_phase_group.jpeg ./plots
+cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group.png ./plots
+cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_group_highlight.png ./plots
+cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_phase.png ./plots
+cp ../experiments/GSE125881/plots/GSE125881_prepare_umap_phase_highlight.png ./plots
+cp ../experiments/GSE125881/plots/GSE125881_prepare_barplot_phase_group.png ./plots
 
 
 echo "GSE136874"
-cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CAR.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CAR_highlight.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_barplot_monaco_CAR.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase_highlight.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_CARTEx_84.jpeg ./plots
 
-cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_predicted_monaco.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_barplot_monaco_CAR_slim.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase.jpeg ./plots
-cp ../experiments/GSE136874/plots/GSE136874_prepare_barplot_phase_CAR_slim.jpeg ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CAR.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CAR_highlight.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_barplot_monaco_CAR.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase_highlight.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_CARTEx_84.png ./plots
+
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_predicted_monaco.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_barplot_monaco_CAR_slim.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_phase.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_barplot_phase_CAR_slim.png ./plots
+
+cp ../experiments/GSE136874/plots/GSE136874_prepare_vlnplot_CAR_exhaustion_markers.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_transition_volcano_GD2vCD19_200.png ./plots
+
+cp ../experiments/GSE136874/plots/GSE136874_featplot_CARTEx_200_CAR.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_CARTEx_200.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_CARTEx_200.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_aggplot_CARTEx_200_CAR_monaco_split_countsized.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_dmap_CAR.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_dmap_CARTEx_200.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_transition_transitplot_CAR_CARTEx_200.png ./plots
+
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_LCMV_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_NKlike_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_BBD_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_umap_PD1_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_scatter_CARTEx_200_LCMV.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_scatter_CARTEx_200_NKlike.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_scatter_CARTEx_200_BBD.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_prepare_scatter_CARTEx_200_PD1.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_LCMV_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_NKlike_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_BBD_Tex.png ./plots
+cp ../experiments/GSE136874/plots/GSE136874_query_agg_aggplot_PD1_Tex.png ./plots
+
+
+
+
+
+
+echo "GSE160160"
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_exposure.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_phase.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_barplot_phase_exposure_slim.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_predicted_monaco.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_predicted_monaco.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_barplot_monaco_exposure_slim.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_vlnplot_exposure_exhaustion_markers.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_explore_volcano_CAEvday0_200.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_featplot_CARTEx_200_exposure.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_CARTEx_200.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_query_agg_aggplot_CARTEx_200.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_aggplot_CARTEx_200_exposure_monaco_split_countsized.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_dmap_exposure.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_dmap_CARTEx_200.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_transition_transitplot_exposure_CARTEx_200.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_LCMV_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_NKlike_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_BBD_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_umap_PD1_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_scatter_CARTEx_200_LCMV.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_scatter_CARTEx_200_NKlike.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_scatter_CARTEx_200_BBD.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_prepare_scatter_CARTEx_200_PD1.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_query_agg_aggplot_LCMV_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_query_agg_aggplot_NKlike_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_query_agg_aggplot_BBD_Tex.png ./plots
+cp ../experiments/GSE160160/plots/GSE160160_query_agg_aggplot_PD1_Tex.png ./plots
+
+
 
 
 
 echo "GSE136184"
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_age.jpeg ./plots
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_age_group_2.jpeg ./plots
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_sex.jpeg ./plots
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_predicted_monaco.jpeg ./plots
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_barplot_monaco_age_group_slim.jpeg ./plots
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_phase.jpeg ./plots
-cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_barplot_phase_age_group_slim.jpeg ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_age.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_age_group_2.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_sex.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_predicted_monaco.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_barplot_monaco_age_group_slim.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_CARTEx_200_monaco.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_phase.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_barplot_phase_age_group_slim.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_CARTEx_200_phase.png ./plots
 
-cp ../experiments/GSE136184/plots/GSE136184_cs_extract_umap_highlight_combined.jpeg ./plots
+
+
+cp ../experiments/GSE136184/plots/GSE136184_cs_extract_umap_highlight_combined.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_umap_CARTEx_200_group.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_prepare_vlnplot_age_group_exhaustion_markers.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_aggplot_CARTEx_200_group_monaco_split_countsized.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_cs_extract_umap_highlight_combined.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_featplot_CARTEx_200_extract_ident.png ./plots
+cp ../experiments/GSE136184/plots/GSE136184_transition_volcano_OTvYN_CARTEx_200.png ./plots
 
 
 
@@ -79,13 +174,13 @@ cp ../experiments/GSE136184/plots/GSE136184_cs_extract_umap_highlight_combined.j
 
 
 echo "Zenodo3993994"
-cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_umap_disease.jpeg ./plots
-cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_dmap_disease.jpeg ./plots
+cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_umap_disease.png ./plots
+cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_dmap_disease.png ./plots
 
-cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_umap_predicted_monaco.jpeg ./plots
-cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_barplot_monaco_disease_slim.jpeg ./plots
-cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_umap_phase.jpeg ./plots
-cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_barplot_phase_disease_slim.jpeg ./plots
+cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_umap_predicted_monaco.png ./plots
+cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_barplot_monaco_disease_slim.png ./plots
+cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_umap_phase.png ./plots
+cp ../experiments/Zenodo3993994/plots/Zenodo3993994_blood_prepare_barplot_phase_disease_slim.png ./plots
 
 
 
